843bd94cfa8d3eeefd8f32f37d482bc56f87f425
Xseba360 <sgladki1998@gmail.com>
2022-09-03 10:41:13 +0200

    typescript

--- cli.ts
+++ cli.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-// noinspection SpellCheckingInspection
-
 'use strict'
+
 process.env.FORCE_COLOR = String(true)
 
 const fs = require('fs')
@@ -14,35 +13,27 @@ const [, , ...args] = process.argv
 // Print args
 console.log('Parsing ', args)
 
-let files = {
-  client: {},
-  server: {},
-  shared: {},
-  native: {}
+let files: Record<ApiTypes, Record<string, string>> = {
+  [ApiTypes.CLIENT]: {},
+  [ApiTypes.SERVER]: {},
+  [ApiTypes.SHARED]: {},
+  [ApiTypes.NATIVE]: {}
 }
 
-/**@type {Map<string,{overload: string, paramsLength: number}>}*/
-const foundNames = new Map()
+const foundNames = new Map<string, FoundName>()
 
-const reservedKeywords = new Set(['and', 'break', 'do', 'else', 'elseif', 'end', 'false', 'for', 'function', 'if', 'in', 'local', 'nil', 'not', 'or', 'repeat', 'return', 'then', 'true', 'until', 'while'])
+const reservedKeywords = new Set<string>(['and', 'break', 'do', 'else', 'elseif', 'end', 'false', 'for', 'function', 'if', 'in', 'local', 'nil', 'not', 'or', 'repeat', 'return', 'then', 'true', 'until', 'while'])
 
 const uppercaseFirst = (str) => str.charAt(0).toUpperCase() + str.slice(1)
 
-/**
- *
- * @param name {string}
- * @return {string}
- */
-const makeNative = (name) => {
+const makeNative = (name: string): string => {
   return uppercaseFirst(name.toLowerCase().replace('0x', 'n_0x').replace(/_([a-z])/g, (regs) => regs[1].toUpperCase()))
 }
 
-/**
- * @param {{results: string, hash: string, params: {name: string, type: string}[], description: string, examples: string, ns: string, apiset: string, name: string, jhash: string}} native
- */
-const renderName = (native) => native.name || native.hash || native.jhash
+const renderName = (native: NativeDeclaration) => native.name || native.hash || native.jhash
 
-function getPrimitiveType (type) {
+
+function getPrimitiveType (type: string) {
   switch (type) {
     case 'int':
     case 'float':
@@ -73,66 +64,72 @@ function getPrimitiveType (type) {
     case 'hash':
     case 'Hash':
     case 'Hash*':
-      return 'number'
+      return PrimitiveTypes.NUMBER
+
     case 'void':
-      return 'void'
+      return PrimitiveTypes.VOID
 
     case 'vector2':
     case 'Vector2':
     case 'Vector2*':
-      return 'vector2'
+      return PrimitiveTypes.VECTOR2
 
     case 'vector3':
     case 'Vector3':
     case 'Vector3*':
-      return 'vector3'
+      return PrimitiveTypes.VECTOR3
 
     case 'vector4':
     case 'Vector4':
     case 'Vector4*':
-      return 'vector4'
+      return PrimitiveTypes.VECTOR4
 
     case 'quat':
     case 'Quat':
     case 'Quat*':
-      return 'quat'
+      return PrimitiveTypes.QUAT
+
     case 'char*':
     case 'char':
-      return 'string'
+      return PrimitiveTypes.STRING
+
+    case 'BOOL*':
     case 'BOOL':
+    case 'bool*':
     case 'bool':
-      return 'boolean'
+      return PrimitiveTypes.BOOLEAN
+
     case 'Any':
     case 'Any*':
-      return 'any'
+      return PrimitiveTypes.ANY
+
     case 'func':
-      return 'function'
+      return PrimitiveTypes.FUNCTION
+
     default:
-      return 'table'
+      return PrimitiveTypes.TABLE
   }
 }
 
-function parseType (type, isReturn, hash) {
+/**
+ * Used for `@overload` and `@param`
+ * @param type
+ * @param isReturn `true` for @overload, `false` for @param
+ * @param hash Function hash
+ */
+function parseType (type: string, isReturn: boolean, hash: string): string {
   let lType
-  // if (type.includes('*') && isReturn !== true) {
-  //     if (type !== 'char*') {
-  //         // i'm guessing it's a multiple return value thing then
-  //         return false
-  //     }
-  // }
   switch (type) {
     case 'int':
     case 'float':
     case 'long':
-      lType = 'number'
-      break
     case 'int*':
     case 'float*':
     case 'long*':
-      lType = 'number'
+      lType = PrimitiveTypes.NUMBER
       break
     case 'void':
-      lType = type.replace('*', '')
+      lType = PrimitiveTypes.VOID
       break
     case 'player':
     case 'Player':
@@ -189,24 +186,24 @@ function parseType (type, isReturn, hash) {
       break
     case 'char*':
     case 'char':
-      lType = 'string'
+      lType = PrimitiveTypes.STRING
       break
     case 'BOOL*':
     case 'BOOL':
     case 'bool*':
     case 'bool':
-      lType = type = 'boolean'
+      lType = type = PrimitiveTypes.BOOLEAN
       break
     case 'Any':
     case 'Any*':
-      lType = type = 'any'
+      lType = type = PrimitiveTypes.ANY
       break
     case 'func':
       lType = type = 'fun'
       break
     case 'object':
       console.info(`\x1b[46m\x1b[30mINFO:\x1b[0m \x1b[36mFound a generic object type ${type} in hash ${hash}\x1b[0m -> ${nativeUrl}${hash}`)
-      lType = 'table'
+      lType = PrimitiveTypes.TABLE
       break
 
     // custom
@@ -220,10 +217,9 @@ function parseType (type, isReturn, hash) {
       break
 
     default:
+      lType = PrimitiveTypes.TABLE
       console.info(`\x1b[41m\x1b[30mERROR:\x1b[0m \x1b[31mPossible unknown type ${type} in hash ${hash}! Fix Me and run again!\x1b[0m`)
-      process.exit()
-
-      lType = 'table'
+      process.exit(1)
   }
   if (isReturn !== true) {
     return lType !== type ? `${lType} (${type})` : type
@@ -232,7 +228,27 @@ function parseType (type, isReturn, hash) {
   }
 }
 
-function isSinglePointerNative (params) {
+/**
+ * Check if the native has ONLY ONE parameter with a pointer.
+ *
+ * Take a look at this native:
+ * [GetVehicleNodePosition](https://docs.fivem.net/natives/?_0x703123E5E7D429C2)
+ *
+ * Raw definition:
+ * ```cpp
+ * void GET_VEHICLE_NODE_POSITION(int nodeId, Vector3* outPosition);
+ * ```
+ * Lua definition:
+ * ```lua
+ * local outPosition --[[ vector3 ]] = GetVehicleNodePosition(nodeId --[[ integer ]])
+ * ```
+ *
+ * Since you can't really do pointers in Lua, the outPosition becomes the return value.
+ * This usually happens in natives with only a single pointer parameter and a `void` retval
+ *
+ * @param params
+ */
+function isSinglePointerNative (params: NativeDelcarationParam[]) {
   let foundPointer = false
   for (const param of params) {
     if (param.type.includes('*') && param.type !== 'char*') {
@@ -246,28 +262,15 @@ function isSinglePointerNative (params) {
   return foundPointer
 }
 
-function parseParams (params, hash, resultsArray) {
+function generateOverload (params: NativeDelcarationParam[], hash: string, resultsArray: string[]): GeneratedOverload {
   let result = ''
   let overload = 'fun('
   let first = true
-  const singlePtr = isSinglePointerNative(params)
-  /*params.forEach(element => {
-      let name = element.name;
-      if (reservedKeywords.has(name)) {
-          console.warn(`Found a reserved keyword: ${name} in hash ${hash}`)
-          name += '_'
-      }
 
-      let type = parseType(element.type);
-      if (type !== false) {
-          result += `--- @param ${name} ${type}\n`;
-      } else {
-          if (resultsArray[0] == 'void') {
-              resultsArray.length = 0 // nasty hack in case the first retval is void
-          }
-          resultsArray.push(parseType(element.type, true))
-      }
-  })*/
+  /** Why is this important?
+   * @see isSinglePointerNative
+   */
+  const singlePtr = isSinglePointerNative(params)
   let overloadEnd = ''
   for (const element of params) {
     if (element.type.includes('*')) {
@@ -306,7 +309,7 @@ function parseParams (params, hash, resultsArray) {
   return [p, overload, singlePtr]
 }
 
-function parseSingleParam (element, hash) {
+function parseSingleParam (element: NativeDelcarationParam, hash: string) {
   let result = ''
   let name = element.name
   if (reservedKeywords.has(name)) {
@@ -321,7 +324,7 @@ function parseSingleParam (element, hash) {
   return result
 }
 
-function parseSingleOverload (element, first, hash) {
+function parseSingleOverload (element: NativeDelcarationParam, first: boolean, hash: string) {
   let result = ''
   let name = element.name
   if (reservedKeywords.has(name)) {
@@ -336,21 +339,11 @@ function parseSingleOverload (element, first, hash) {
   return result
 }
 
-function parseParamsMethod (params) {
+function parseParamsMethod (params: NativeDelcarationParam[]) {
   let result = ''
   let first = true
   const singlePtr = isSinglePointerNative(params)
   params.forEach(element => {
-//         if (!(element.type.includes('*'))) {
-//             if(!first) result += ', ';
-//             result += element.name;
-
-//             if (reservedKeywords.has(element.name)) {
-//                 result += '_'
-//             }
-//             first = false;
-//         }
-
     if (element.type.includes('*')) {
       if (singlePtr) {
         if (!first) result += ', '
@@ -383,7 +376,7 @@ function parseParamsMethod (params) {
   return result
 }
 
-function parseExamples (examples) {
+function parseExamples (examples: NativeDeclarationExample[]): string {
   let result = ''
   examples.forEach(example => {
     if (example.lang === 'lua') {
@@ -394,12 +387,16 @@ function parseExamples (examples) {
   return result.length > 0 ? result.substring(0, result.length - 1) : '---'
 }
 
-function parseDescription (name, desc) {
+function parseDescription (name: string, desc: string) {
   desc = desc.replaceAll('](#\\_0x', `](${nativeUrl}0x`)
   return '--- ' + (desc.length > 0 ? desc.replace(/\n/g, '\n--- ') : name)
 }
 
-function getTemporaryManualOverload (apiset, namespace, name) {
+/**
+ * Get a manual override for a function (in case there's something that needs to be
+ * tweaked, and it isn't tweaked in the official natives)
+ */
+function getTemporaryManualOverload (apiset: string, namespace: string, name: string) {
   let manual = ''
   switch (name) {
     case 'GetVehicleLockOnTarget':
@@ -431,6 +428,7 @@ function getTemporaryManualOverload (apiset, namespace, name) {
       manual = 'fun(vehicle: Vehicle, player: Player): boolean'
       break
     case 'DrawMarker':
+      // make last param three params optional
       manual = 'fun(type: number, posX: number, posY: number, posZ: number, dirX: number, dirY: number, dirZ: number, rotX: number, rotY: number, rotZ: number, scaleX: number, scaleY: number, scaleZ: number, red: number, green: number, blue: number, alpha: number, bobUpAndDown: boolean, faceCamera: boolean, p19: number, rotate: boolean): void'
       break
     case 'SetFacialIdleAnimOverride':
@@ -442,59 +440,70 @@ function getTemporaryManualOverload (apiset, namespace, name) {
   return manual
 }
 
-function fixMethod (methodObj) {
-  if (methodObj.hash === '0x9B8E1BF04B51F2E8') {
-    // GetAllVehciles() should only be used on server, on client GetGamePool() should be used.
-    return false
+/**
+ * Perform simple fixes on a method
+ * @return Should the method be considered for further processing or discarded? (`false` = discard)
+ */
+function fixMethod (methodObj: NativeDeclaration) {
+  switch (methodObj.hash) {
+    case '0x9B8E1BF04B51F2E8':
+      // GetAllVehicles() should only be used on server, on client GetGamePool() should be used.
+      return false
 
-  } else if (methodObj.hash === '0x760A2D67') {
-    methodObj.results = 'number[]'
+    case '0x760A2D67':
+      methodObj.results = 'number[]'
+      break
 
-  } else if (methodObj.hash === '0xCF143FB9') {
-    methodObj.results = 'Player[]'
+    case '0xCF143FB9':
+      methodObj.results = 'Player[]'
+      break
 
-  } else if (methodObj.hash === '0x6886C3FE') {
-    methodObj.results = 'Object[]'
+    case '0x6886C3FE':
+      methodObj.results = 'Object[]'
+      break
 
-  } else if (methodObj.hash === '0xF65BBA4B') {
-    methodObj.results = '{doorHash: Hash, doorHandle: number}[]'
+    case '0xF65BBA4B':
+      methodObj.results = '{doorHash: Hash, doorHandle: number}[]'
+      break
 
-  } else if (methodObj.hash === '0xB8584FEF') {
-    methodObj.results = 'Ped[]'
+    case '0xB8584FEF':
+      methodObj.results = 'Ped[]'
+      break
 
-  } else if (methodObj.hash === '0x332169F5') {
-    methodObj.results = 'Vehicle[]'
+    case '0x332169F5':
+      methodObj.results = 'Vehicle[]'
+      break
 
-  } else if (methodObj.hash === '0x2B9D4F50') {
-    methodObj.results = '(Ped|Object|Vehicle|Pickup)[]'
+    case '0x2B9D4F50':
+      methodObj.results = '(Ped|Object|Vehicle|Pickup)[]'
+      break
 
-  } else if (methodObj.hash === '0xD4BEF069') {
-    methodObj.results = 'RegisteredCommandInfo[]'
-
+    case '0xD4BEF069':
+      methodObj.results = 'RegisteredCommandInfo[]'
+      break
   }
   return true
 }
 
 /**
- *
- * @param methodObj {{results: string, hash: string, params: [{name: string, type: string}], description: string, examples: string, ns:string, apiset: string, name: string, jhash: string, aliases: string[]}}
- * @return {[{namespace: string, apiset: string, content: string}]}
+ * Main parsing method
+ * @param methodObj
  */
-function parseMethod (methodObj) {
+function parseMethod (methodObj: NativeDeclaration): ParsedMethod[]|null {
   if (!fixMethod(methodObj)) {
     return null
   }
-  let name = makeNative(renderName(methodObj))
-  let hash = methodObj.hash
-  let resultsArray = [parseType(methodObj.results, true, hash)]
-  let [params, overload, singlePtr] = parseParams(methodObj.params, methodObj.hash, resultsArray)
-  let paramsMethod = parseParamsMethod(methodObj.params)
-  let description = parseDescription(name, methodObj.description)
-  let examples = parseExamples(methodObj.examples)
+  const name = makeNative(renderName(methodObj))
+  const hash = methodObj.hash
+  const resultsArray: string[] = [parseType(methodObj.results, true, hash)]
+  let [params, overload, singlePtr] = generateOverload(methodObj.params, methodObj.hash, resultsArray)
+  const paramsMethod = parseParamsMethod(methodObj.params)
+  const description = parseDescription(name, methodObj.description)
+  const examples = parseExamples(methodObj.examples)
 
-  let namespace = makeNative(methodObj.ns)
-  let apiset = methodObj.apiset
-  let manualOverload = getTemporaryManualOverload(apiset, namespace, name)
+  const namespace = makeNative(methodObj.ns)
+  const apiset = methodObj.apiset
+  const manualOverload = getTemporaryManualOverload(apiset, namespace, name)
   let results = ''
   if (!singlePtr) {
     overload += ': '
@@ -515,7 +524,7 @@ function parseMethod (methodObj) {
     // console.error(`Discarding a duplicate method definition: ${name} (hash: ${methodObj.hash}, apiset: ${methodObj.apiset})`)
 
     if (overload !== foundNames.get(name).overload && manualOverload.length <= 0 && methodObj.params.length !== foundNames.get(name).paramsLength) {
-      console.error(`\n\x1b[43m\x1b[30mWARN:\x1b[0m \x1b[33mapiset: ${methodObj.apiset} - namespace: ${namespace} - Discarding a duplicate method definition: ${name} (hash: ${methodObj.hash})\x1b[0m`)
+      console.error(`\n\x1b[43m\x1b[30mWARN:\x1b[0m\x1b[33m apiset: ${methodObj.apiset} - namespace: ${namespace} - Discarding a duplicate method definition: ${name} (hash: ${methodObj.hash})\x1b[0m`)
       console.error('discarded:', overload)
       console.error('generated:', foundNames.get(name).overload, '\n')
     }
@@ -529,8 +538,9 @@ function parseMethod (methodObj) {
   if (manualOverload.length > 0) {
     overload = manualOverload
   }
-  const output = [{
-    namespace, apiset,
+  const output: ParsedMethod[] = [{
+    namespace,
+    apiset,
     content: `
 ${description}
 ${examples}
@@ -545,7 +555,8 @@ function ${name}(${paramsMethod}) end
   if (methodObj.aliases !== undefined) {
     for (const alias of methodObj.aliases) {
       output.push({
-        namespace, apiset,
+        namespace,
+        apiset,
         content: `
 --- # New Name: ${name}
 ${description}
@@ -564,19 +575,29 @@ function ${makeNative(alias)}(${paramsMethod}) end
   return output
 }
 
-function parse (key, methodObj) {
-  let datum = parseMethod(methodObj)
-  if (datum !== null) {
-    for (const data of datum) {
-      let api = data.apiset ? data.apiset.toLowerCase() : 'native'
-      let namespace = data.namespace ? data.namespace.toLowerCase() : 'default'
+function parse (key: string, methodObj: NativeDeclaration) {
+  let methods: ParsedMethod[] = parseMethod(methodObj)
+  if (methods !== null) {
+    for (const method of methods) {
+      let api = method.apiset ? method.apiset.toLowerCase() : 'native'
+      let namespace = method.namespace ? method.namespace.toLowerCase() : 'default'
       let file = files[api][namespace]
       if (!file) {
         file = ''
       }
-      files[api][namespace] = file + data.content
+      files[api][namespace] = file + method.content
     }
   }
+}
+
+function write (fileName: string, contents) {
+  fs.writeFile('docs-gen/' + fileName + '.def.lua', contents, (err) => {
+    // throws an error, you could also catch it here
+    if (err) throw err
+
+    // success case, the file was saved
+    console.log(fileName + ' saved!')
+  })
 }
 
 args.forEach(file => {
@@ -590,16 +611,6 @@ args.forEach(file => {
     }
   }
 })
-
-function write (fileName, contents) {
-  fs.writeFileSync('docs-gen/' + fileName + '.def.lua', contents, (err) => {
-    // throws an error, you could also catch it here
-    if (err) throw err
-
-    // success case, the file was saved
-    console.log(fileName + ' saved!')
-  })
-}
 
 for (const api in files) {
   for (const namespace in files[api]) {
---
+++ global.d.ts
@@ -0,0 +1,58 @@
+declare const enum ApiTypes {
+  CLIENT = 'client',
+  SERVER = 'server',
+  SHARED = 'shared',
+  NATIVE = 'native',
+}
+
+declare const enum PrimitiveTypes {
+  NUMBER = 'number',
+  VOID = 'void',
+  VECTOR2 = 'vector2',
+  VECTOR3 = 'vector3',
+  VECTOR4 = 'vector4',
+  QUAT = 'quat',
+  STRING = 'string',
+  BOOLEAN = 'boolean',
+  ANY = 'any',
+  FUNCTION = 'function',
+  TABLE = 'table',
+}
+
+
+declare type FoundName = {
+  overload: string
+  paramsLength: number
+}
+
+declare type NativeDelcarationParam = {
+  name: string
+  type: string
+}
+
+declare type NativeDeclarationExample = {
+  lang: string,
+  code: string
+}
+
+declare type NativeDeclaration = {
+  results: string
+  hash: string
+  params: NativeDelcarationParam[]
+  description: string
+  examples: NativeDeclarationExample[]
+  aliases?: string[]
+  ns: string
+  apiset: string
+  name: string
+  jhash?: string
+  manualHash?: boolean
+}
+
+declare type GeneratedOverload = [string, string, boolean]
+
+declare type ParsedMethod = {
+  namespace: string
+  apiset: string
+  content: string
+}
