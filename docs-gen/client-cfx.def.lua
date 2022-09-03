0bf31363920e1955e97485fdc65cd45d7e72dfd3
Xseba360 <sgladki1998@gmail.com>
2022-09-03 16:06:32 +0200

    test

--- docs-gen/client-cfx.def.lua
+++ docs-gen/client-cfx.def.lua
@@ -2150,6 +2150,11 @@ function GetInteriorPortalEntityArchetype(interiorId, portalIndex, entityIndex) 
 --- @overload fun(submixId: number, effectSlot: number, paramIndex: number, paramValue: number): void
 function SetAudioSubmixEffectParamFloat(submixId, effectSlot, paramIndex, paramValue) end
 
+
+
+
+
+
     
 --- GetVehicleDashboardSpeed
 ---
